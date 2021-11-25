0fe230992d7bd9983c93bac31f13217db77a9532
LukasBasiura <basiura.lukas@gmail.com>
2021-11-25 11:04:18 +0100

    Fix problem with mapping CountryDto by ModelMapper

--- src/main/java/com/example/exampleproject/model/apidto/CountryDto.java
+++ src/main/java/com/example/exampleproject/model/apidto/CountryDto.java
@@ -1,4 +1,59 @@
 package com.example.exampleproject.model.apidto;
 
-public record CountryDto(Long id, String name, String code, String flag) implements ICountry {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public final class CountryDto implements ICountry {
+    private Long id;
+    private String name;
+    private String code;
+    private String flag;
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public String flag() {
+        return flag;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (CountryDto) obj;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.name, that.name) &&
+                Objects.equals(this.code, that.code) &&
+                Objects.equals(this.flag, that.flag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, code, flag);
+    }
+
+    @Override
+    public String toString() {
+        return "CountryDto[" +
+                "id=" + id + ", " +
+                "name=" + name + ", " +
+                "code=" + code + ", " +
+                "flag=" + flag + ']';
+    }
+
 }
